691c80f2ee247fdf681228a37089228d7ea7a5d1
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-01-31 22:38:34 +0530

    Changed UI

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.white),
+      ),
       debugShowCheckedModeBanner: false,
       title: "Smart Reserve",
       home: SplashScreen(),
