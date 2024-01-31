655c2f2c608b94efbc92de170f10efe04d0aa614
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-01-31 22:44:58 +0530

    Modified UI

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -37,7 +37,7 @@ class _SplashScreenState extends State<SplashScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text("Smart Class Room Booking",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24.0),),
-                Text("v1.0.0"),
+                Text("v1.0.0 - STABLE"),
               ],
             ),
           ),
