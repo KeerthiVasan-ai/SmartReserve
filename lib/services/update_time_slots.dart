d628ffc3621a976ea7ceb4079595a41e75a8e5bc
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-02-16 22:03:28 +0530

    Slot Deletion Logic

--- lib/services/update_time_slots.dart
+++ lib/services/update_time_slots.dart
@@ -1,11 +1,12 @@
 import "package:cloud_firestore/cloud_firestore.dart";
 import 'dart:developer' as dev;
+import 'package:intl/intl.dart';
 
 
 class UpdateTimeSlots {
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  static Future<void> updateData(String date,List<String> selectedSlots) async {
+  static Future<void> insertSlots(String date,List<String> selectedSlots,bool available) async {
     try {
       DocumentReference documentReference = _firestore.collection("timeSlots")
           .doc(date)
@@ -15,7 +16,7 @@ class UpdateTimeSlots {
       Map<String,dynamic> data = snapshot.data() as Map<String,dynamic>;
 
       for(String slots in selectedSlots){
-        data[slots] = false;
+        data[slots] = available;
       }
 
       await documentReference.update(data);
@@ -25,4 +26,30 @@ class UpdateTimeSlots {
     }
   }
 
+  static Future<void> deleteSlot(String dateFromActivity,List<dynamic> selectedSlots,bool available) async {
+
+    try {
+
+      DateTime myDate = DateFormat("dd-MM-yyyy").parse(dateFromActivity);
+      String date = DateFormat('yyyy-MM-dd').format(myDate);
+      dev.log(date,name: "Date");
+
+      DocumentReference documentReference = _firestore.collection("timeSlots")
+          .doc(date)
+          .collection("availability")
+          .doc("slots");
+
+      DocumentSnapshot snapshot = await documentReference.get();
+      Map<String,dynamic> data = snapshot.data() as Map<String,dynamic>;
+
+      for(String slots in selectedSlots){
+        data[slots] = available;
+      }
+
+      await documentReference.update(data);
+      dev.log("Success",name:"Message");
+    } catch (e){
+      dev.log(e.toString(),name:"Error");
+    }
+  }
 }
