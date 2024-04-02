92f80b5c09bb503de9e782cf63c665b5007650ff
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-04-02 07:51:46 +0530

    Changed Validation Constraints

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -33,11 +33,14 @@ class _LoginScreenState extends State<LoginScreen> {
             email: userName.text.trim(), password: password.text);
         Navigator.pop(context);
       } on FirebaseAuthException catch (e) {
+        print(e.code.toString());
         Navigator.pop(context);
-        if (e.code == 'invalid-credential') {
-          print('No user found for that email.');
-        } else if (e.code == 'wrong-password') {
-          print('Wrong password provided for that user.');
+        if (e.code == 'invalid-email') {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text("Invalid Email")));
+        } else if (e.code == 'invalid-credential') {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text("Check your Credentials")));
         }
       } catch (e) {
         print(e.toString());
